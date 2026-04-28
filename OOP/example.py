ec07ccf6cfd4638a716b0d26cc5387c62293df0f
Arsen Klevtsovskyi <avklevtsovskiy@gmail.com>
2026-04-28 10:11:46 +0300

    example tkinter

--- OOP/example.py
+++ OOP/example.py
@@ -1,27 +1,19 @@
-class A:
+from tkinter import *
 
-    def __init__(self, a, b, c):
-        self.a = a
-        self._b = b
-        self.__c = c
+root = Tk()
 
-    def get(self):
-        return f"{self.a} {self._b} {self.__c}"
+entry = Entry(root, font="Arial 24")
+entry.pack()
+label = Label(root, font="Arial 24", text="Hello!")
+label.pack()
 
-    def show(self):
-        print(f"A: {self.get()}")
-
-
-class B(A):
-
-    def show(self):
-        print(f"B: {self.get()}")
-        super().show()
+def callback():
+    value = entry.get()
+    label["text"] = value
 
 
-if __name__ == '__main__':
-    obj_a = A(432, 542, 103)
-    obj_a.show()
+btn1 = Button(root, text="Hello!", font="Arial 24", command=callback)
+btn1.pack()
 
-    obj_b = B(123, 234, 654)
-    obj_b.show()
+
+root.mainloop()
